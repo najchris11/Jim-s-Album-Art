15c57bf87c6f420ad3f1d62a85c95f3af3008c06
Najib <52837430+najchris11@users.noreply.github.com>
2024-02-13 11:52:43 -0500

    basics of css styling in next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
 function HomePage() {
-    return <div>Welcome to Jim's Robot Shop!</div>
+    const ctaStyles = {
+        display: 'flex',
+        justifyContent: 'space-around'
+    }
+    return (
+    <div style={ctaStyles}>
+        <span>
+            Sample Image 1
+        </span>
+        <span>
+            Sample Image 2
+        </span>
+        <div>
+            <div>
+                Your <em>Album</em> and <em>Artist</em> Name
+            </div>
+            <div>
+                <div>on <em>Custom</em> Album Designs</div>
+            </div>
+            <button>Shop Now</button>
+        </div>
+        <span>
+            Sample Image 3
+        </span>
+        <span>
+            Sample Image 4
+        </span>
+    </div>
+    )
 }
 
 export default HomePage
