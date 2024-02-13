37685875ab71bedff51596984a8a233be7db7b7e
Najib <52837430+najchris11@users.noreply.github.com>
2024-02-13 15:14:25 -0500

    basic image importing and handling by next

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,39 @@
+import Image from 'next/image'
 import styles from './HomePage.module.scss'
+import {
+    divinity,
+    synchronicity,
+    theUndertaking, 
+    asTheSunSpeaks
+} from '../public/images/albums'
+import divinityImage from '../public/images/albums/sunset-divide/divinity.png'
 function HomePage() {
     return (
-    <div className={styles.cta}>
-        <div className={styles.header}>Home Header</div>
-        <span>
-            Sample Image 1
-        </span>
-        <span>
-            Sample Image 2
-        </span>
-        <div>
-            <div>
-                Your <em>Album</em> and <em>Artist</em> Name
+        <div className={styles.cta}>
+            {/* <div className={styles.header}>Home Header</div> */}
+            <span>
+                {/* Using Image requires that I either have a fill aspect, or that I set the width and height */}
+                <Image width='200' height='200' src='/images/albums/sunset-divide/divinity.png' />
+            </span>
+            <span className={styles.image}> {/*this doesn't work nearly as efficiently bc it's sized by the css*/}
+                <Image src={divinity} />
+            </span>
+            <div className={styles.ctaText}>
+                <div className={styles.ctaMainText}>
+                    Your <em>Album</em> and <em>Artist</em> Name
+                </div>
+                <div className={`${styles.ctaSubText}`}>
+                    <div>on <em>Custom</em> Album Designs</div>
+                </div>
+                <button class='cta'>Shop Now</button>
             </div>
-            <div>
-                <div>on <em>Custom</em> Album Designs</div>
-            </div>
-            <button class='cta'>Shop Now</button>
+            <span>
+                <Image width='200' height='200' src={divinityImage} />
+            </span>
+            <span>
+                <Image width='200' height='200' src='/images/albums/sunset-divide/divinity.png' />
+            </span>
         </div>
-        <span>
-            Sample Image 3
-        </span>
-        <span>
-            Sample Image 4
-        </span>
-    </div>
     )
 }
 
